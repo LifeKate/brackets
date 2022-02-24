449de566af60b2a01a9ac4e054f4865d5e2dd9eb
Katsiarina Herasimenka <asadchaaekaterina0@gmail.com>
2022-02-24 22:15:14 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var result = false;
+
+  str = str.split("");
+
+  var m = 1;
+
+  outer: while (m < 100) {
+    for (var j = 0; j < bracketsConfig.length; j++)
+      for (var i = 0; i < str.length; i++) {
+        if (
+          str[i] == bracketsConfig[j][1] &&
+          str[i - 1] == bracketsConfig[j][0]
+        ) {
+          str.splice(i - 1, 2);
+        }
+      }
+    if (str.length == 0) {
+      result = true;
+      break outer;
+    }
+    m++;
+  }
+
+  return result;
+};
